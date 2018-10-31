d5b98246e422ffa591970f7adb783cf180fef830
Joacim Åström <joacim.astrom@gmail.com>
2018-10-31 14:11:41 +0100

    Backend support for billing

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,3 +1,17 @@
+const stripe = require('stripe')(require('../config/keys').stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
+
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {});
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id
+    });
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    return res.send(user);
+  });
 };
